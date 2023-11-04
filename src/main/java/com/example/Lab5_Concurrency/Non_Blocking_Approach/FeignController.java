77597fa9f0c16a966f8f193e594181303064c189
Parthibg00410769 <g00410769@atu.ie>
2023-11-04 12:43:25 +0000

    Code has been committed for extracting the TodoList with the specific requested ID

--- src/main/java/com/example/Lab5_Concurrency/Non_Blocking_Approach/FeignController.java
+++ src/main/java/com/example/Lab5_Concurrency/Non_Blocking_Approach/FeignController.java
@@ -1,37 +1,19 @@
 package com.example.Lab5_Concurrency.Non_Blocking_Approach;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class FeignController {
-    private final FeignService feignService;
+    private final TodoClient todoClient;
 
-    public FeignController(FeignService feignService){
-        this.feignService = feignService;
+    public FeignController(TodoClient todoClient) {
+        this.todoClient = todoClient;
     }
 
-    @GetMapping("/feign")
-    public String getFeignData() throws Exception, InterruptedException{
-        long startTime = System.currentTimeMillis();
-
-        List<CompletableFuture<TodoResponse>> futures = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++){
-            CompletableFuture<TodoResponse> future = CompletableFuture.supplyAsync(() -> feignService.fetchData());
-            futures.add(future);
-        }
-
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-
-        allOf.get();
-        long endTime = System.currentTimeMillis();
-        return "Total execution time: " + (endTime - startTime) + " ms";
-
-
+    @GetMapping("/feign/todo/{id}")
+    public TodoResponse getFeignTodoById(@PathVariable int id) {
+        return todoClient.fetchTodoById(id);
     }
 }
